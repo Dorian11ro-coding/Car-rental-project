ec6fa8f525bd290fa02b2905af48759a969be86b
Dorian <minimickey427@yahoo.com>
2024-01-22 22:11:55 +0200

    Initial Commit

---
+++ Car rental project/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Car rental project
+//
+//  Created by Scarlat  Dorian Andrei on 22.01.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
